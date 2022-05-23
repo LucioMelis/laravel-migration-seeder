64cf6a9dd19e6b17a5c3dd2776c264d041efdd24
luciomelis <luciomelis1992@gmail.com>
2022-05-23 18:52:03 +0200

    Ho appena modificato il phpmyadmin inserendo i dati nel database

--- laravel-migration-seeder/app/Train.php
+++ laravel-migration-seeder/app/Train.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Train extends Model
 {
-
+ /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'trains';
 }
